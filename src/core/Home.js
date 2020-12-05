01220934c81df98f6d6cce153e4f4d31c7baa0f9
greeshmasunil10 <greeshmasunil10@gmail.com>
2020-12-04 22:20:33 -0500

    init

---
+++ src/core/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Layout from "./Layout";
+
+const Home = () => (
+  <Layout title="Home Page" description="Winter Store. An eCommerce App">
+    ...
+  </Layout>
+);
+
+export default Home;
